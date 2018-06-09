c6460dfe39ef5b5ae17b98afb72888abf9a545fd
Wilson Berkow <wilsonberkow@gmail.com>
2018-06-09 16:49:07 -0400

    Add Counter.elm example

---
+++ elm-examples/src/Counter.elm
@@ -0,0 +1,77 @@
+module Counter exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (onClick)
+
+
+-- WHOMST
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { count : Int
+    }
+
+
+init : Model
+init =
+    { count = 0
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | count = increment model.count }
+
+        Decrement ->
+            { model | count = decrement model.count }
+
+
+decrement x =
+    x - 1
+
+
+increment : Int -> Int
+increment x =
+    x + 1
+
+
+reset _ =
+    init.count
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model.count) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
