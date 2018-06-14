e74cd9957f00e2f923cd0034e2f8065b4d8cb09f
William Brandon <hypercube97@gmail.com>
2018-06-14 23:18:40 +0000

    Add stub implementations of instrumentation functions

--- reader/src/Elm/Kernel/Reader.js
+++ reader/src/Elm/Kernel/Reader.js
@@ -1,8 +1,34 @@
 /*
 
+import Elm.Kernel.Utils exposing (Tuple0)
+
 */
 
-var _Reader_read = F3(function (funcName, range, value) {
-    console.log("Elm.Kernel.Reader.read: ", funcName, "\n  from: ", range, "\n  result: ", value);
-    return value;
+var _Reader_recordExpr = F2(function(exprId, val)
+{
+    // TODO: Record expression!
+    return val;
+});
+
+var _Reader_recordCall = F3(function(exprId, func, body)
+{
+    // TODO: Record call!
+    return body(__Utils_Tuple0);
+});
+
+var _Reader_recordFrame = F2(function(frameId, body)
+{
+    // TODO: Record frame!
+    return body(__Utils_Tuple0);
+});
+
+var _Reader_markInstrumented = function(func)
+{
+    // TODO: Mark instrumented!
+    return func;
+};
+
+var _Reader_seq = F2(function(sideEffect, val)
+{
+    return val;
 });
--- reader/src/Reader.elm
+++ reader/src/Reader.elm
@@ -1,21 +1,41 @@
-module Reader exposing (read)
+module Reader exposing (recordExpr, recordCall, recordFrame, markInstrumented, seq)
 
 {-|
 
     Reader.
 
-    @docs read
+    @docs recordExpr, recordCall, recordFrame, markInstrumented, seq
 
 -}
 
 import Elm.Kernel.Reader
 
 
-{-|
+{-| -}
+recordExpr : Int -> a -> a
+recordExpr =
+    Elm.Kernel.Reader.recordExpr
 
-    Reader.read
 
--}
-read : String -> Int -> a -> a
-read =
-    Elm.Kernel.Reader.read
+{-| -}
+recordCall : Int -> (a -> b) -> (() -> c) -> c
+recordCall =
+    Elm.Kernel.Reader.recordCall
+
+
+{-| -}
+recordFrame : String -> (() -> a) -> a
+recordFrame =
+    Elm.Kernel.Reader.recordFrame
+
+
+{-| -}
+markInstrumented : (a -> b) -> (a -> b)
+markInstrumented =
+    Elm.Kernel.Reader.markInstrumented
+
+
+{-| -}
+seq : a -> b -> b
+seq =
+    Elm.Kernel.Reader.seq
