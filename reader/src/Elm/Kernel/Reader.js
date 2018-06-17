8658971c3f2cfe8a92a2b16ed56313fd4541ef02
William Brandon <hypercube97@gmail.com>
2018-06-17 03:37:17 +0000

    Initial implementation of kernel functions

--- reader/src/Elm/Kernel/Reader.js
+++ reader/src/Elm/Kernel/Reader.js
@@ -4,31 +4,161 @@ import Elm.Kernel.Utils exposing (Tuple0)
 
 */
 
+var _Reader_context = {
+  $: __1_NON_INSTRUMENTED_FRAME,
+  __childFrames: [],
+};
+
 var _Reader_recordExpr = F2(function(exprId, val)
 {
-    // TODO: Record expression!
-    return val;
+  if (_Reader_context.$ !== __1_INSTRUMENTED_FRAME)
+  {
+    console.warn(
+      'Elm Reader: Someone tried to record an expression from a non-instrumented context.',
+      'expression id: ', exprId,
+      'value: ', val
+    );
+  }
+  else
+  {
+    _Reader_context.__exprs[exprId] = {
+      __val: val,
+      __childFrame: null,
+    };
+  }
+
+  return val;
 });
 
 var _Reader_recordCall = F3(function(exprId, func, body)
 {
-    // TODO: Record call!
+  if (_Reader_context.$ !== __1_INSTRUMENTED_FRAME)
+  {
+    console.warn(
+      'Elm Reader: Someone tried to record a function call from a non-instrumented context.',
+      'expression id: ', exprId,
+      'value: ', val
+    );
     return body(__Utils_Tuple0);
+  }
+
+  var newContext;
+  if (!body.elmReaderInstrumented)
+  {
+    var newContext = {
+      $: __1_NON_INSTRUMENTED_FRAME,
+      __childFrames: [],
+    }
+
+    var oldContext = _Reader_context;
+    _Reader_context = newContext;
+    var result = body(__Utils_Tuple0);
+    _Reader_context = oldContext;
+
+    _Reader_context.__exprs[exprId] = {
+      __val: result,
+      __childFrame: {
+        $: __2_NON_INSTRUMENTED,
+        __childFrames: newContext.__childFrames,
+      },
+    };
+
+    return result;
+  }
+  else
+  {
+    var newContext = {
+      $: 'Call',
+      __childFrame: null,
+    };
+
+    var oldContext = _Reader_context;
+    _Reader_context = newContext;
+    var result = body(__Utils_Tuple0);
+    _Reader_context = oldContext;
+
+    _Reader_context.__exprs[exprId] = {
+      __val: result,
+      __childFrame: newContext.__childFrame,
+    };
+
+    return result;
+  }
 });
 
 var _Reader_recordFrame = F2(function(frameId, body)
 {
-    // TODO: Record frame!
-    return body(__Utils_Tuple0);
+  var newContext = {
+    $: __1_INSTRUMENTED_FRAME,
+    __exprs: [],
+  };
+
+  var oldContext = _Reader_context;
+  _Reader_context = newContext;
+  var result = body(__Utils_Tuple0);
+  _Reader_context = oldContext;
+
+  var newFrame = {
+    $: __2_INSTRUMENTED,
+    __id: frameId,
+    __exprs: newContext.__exprs,
+  };
+
+  if (_Reader_context.$ === 'Call')
+  {
+    _Reader_context.__childFrame = newFrame;
+  }
+  else if (_Reader_context.$ === __1_NON_INSTRUMENTED_FRAME)
+  {
+    _Reader_context.__childFrames.push(newFrame);
+  }
+
+  return result;
 });
 
 var _Reader_markInstrumented = function(func)
 {
-    // TODO: Mark instrumented!
-    return func;
+  func.elmReaderInstrumented = true;
+  return func;
 };
 
 var _Reader_seq = F2(function(sideEffect, val)
 {
-    return val;
+  return val;
 });
+
+// Tools for debugging the debugger from the JavaScript console
+
+function _Reader_toHumanReadable(frame)
+{
+  if (frame.$ === __2_INSTRUMENTED)
+  {
+    return {
+      tag: 'Instrumented',
+      id: frame.__id,
+      exprs: frame.__exprs.map(function(expr) {
+        var childFrame = null;
+        if (expr.__childFrame !== null) {
+          childFrame = _Reader_toHumanReadable(expr.__childFrame);
+        }
+        return {
+          val: expr.__val,
+          childFrame: childFrame,
+        };
+      }),
+    };
+  }
+  else
+  {
+    return {
+      tag: 'NonInstrumented',
+      childFrames: frame.__childFrames.map(_Reader_toHumanReadable),
+    };
+  }
+}
+
+// Need to export as global to be available from the console
+window.elmReaderRoot = function()
+{
+  return _Reader_toHumanReadable(_Reader_context);
+};
